d7ea5e879a1beb2a38ec092e50fc9ae3863cac49
ichigo-dev <ichigo@web-freework.com>
2021-04-04 23:43:22 +0900

    update html/templates

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "net/http"
   "github.com/julienschmidt/httprouter"
+  "html/template"
 )
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -14,7 +15,13 @@ func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 func process(w http.ResponseWriter, r *http.Request) {
   r.ParseForm()
-  fmt.Fprintln(w, r.Form["hello"])
+  fmt.Fprintln(w, r.PostForm["hello"])
+  fmt.Fprintln(w, r.FormValue("hello"))
+}
+
+func client(w http.ResponseWriter, r *http.Request) {
+  t, _ := template.ParseFiles("/templates/client.html")
+  t.Execute(w, "Hello")
 }
 
 func main() {
@@ -24,6 +31,7 @@ func main() {
     Addr: "127.0.0.1:8080",
   }
   http.HandleFunc("/process", process)
+  http.HandleFunc("/client", client)
 
   server.ListenAndServe()
 }
