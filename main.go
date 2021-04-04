d820b6201e58d23285e360e10f69522e0e389cfc
ichigo-dev <ichigo@web-freework.com>
2021-04-04 23:14:55 +0900

    update Form

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import (
   "net/http"
   "github.com/julienschmidt/httprouter"
 )
+
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Fprintf(w, "Hello World, %s", p.ByName("name"))
+  h := r.Header
+  fmt.Fprintln(w, h)
+  // fmt.Fprintf(w, "Hello World, %s", p.ByName("name"))
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+  fmt.Fprintln(w, r.Form["hello"])
 }
 
 func main() {
@@ -14,8 +22,8 @@ func main() {
   mux.GET("/hello/:name", hello)
   server := http.Server{
     Addr: "127.0.0.1:8080",
-    Handler: mux,
   }
+  http.HandleFunc("/process", process)
 
   server.ListenAndServe()
 }
